1ad348f3e54b55a2ed40f0e45f0980746ee246d5
c_du <102283742+charlottedu1031@users.noreply.github.com>
2024-08-01 16:23:16 -0400

    add buttons and img to home

---
+++ src/components/HeroImg.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./HeroImgStyles.css"
+import IntroImg from "../assets/intro.jpg"
+import { Link } from "react-router-dom"
+
+
+const HeroImg = () => {
+	return (
+		<div className="hero">
+			<div className="mask">
+				<img className="intro-img" src={IntroImg} alt="background" />
+			</div>
+			<div className='content'>
+				<p>Charlotte Du</p>
+				<h1>Full Stack Developer</h1>
+				<div>
+					<Link to="/project" className='btn'>Projects</Link>
+					<Link to="/contact" className='btn btn-light'>Contact</Link>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default HeroImg

d8a9ebf8e46c061149b4052961c8c679180e7868
c_du <102283742+charlottedu1031@users.noreply.github.com>
2024-08-02 13:36:08 -0400

    add footer component to pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,19 @@ const Navbar = () => {
 	const [click, setClick] = useState(false)
 	const hanleClick = () => setClick(!click)
 
+	const [bgColor, setBgColor] = useState(false)
+
+	const addBgColor = () => {
+		if (window.scrollY >= 100) {
+			setBgColor(true)
+		} else {
+			setBgColor(false)
+		}
+	}
+
+	window.addEventListener("scroll", addBgColor)
 	return (
-		<div className="header">
+		<div className={bgColor ? "header header-bg" : "header"}>
 			<Link to="/">
 				<h1>Portfolio</h1>
 			</Link>
